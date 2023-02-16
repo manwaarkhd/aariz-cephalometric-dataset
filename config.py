907e91cc9ce3c3528dde214a24d5ce10c442546e
Muhammad Anwaar Khalid <manwaarkhalid@gmail.com>
2023-02-16 15:50:28 +0500

    Dataset reading files
    In this commit we have added scripts to read the cephalograms and their corresponding annotations from the respective folders.

---
+++ config.py
@@ -0,0 +1,148 @@
+ANATOMICAL_LANDMARKS = {
+    "ckr20ld7v4a4r0za3gqkydid2": {
+        "title": "A-point",
+        "symbol": "A"
+    },
+    "ckr6rysyb3jyi0y303gbq84ks": {
+        "title": "Anterior Nasal Spine",
+        "symbol": "ANS"
+    },
+    "ckr6rysyb3jyk0y301dcyhf9q": {
+        "title": "B-point",
+        "symbol": "B"
+    },
+    "ckr6rysyc3jym0y307w5s1g4a": {
+        "title": "Menton",
+        "symbol": "Me"
+    },
+    "ckr6rysyc3jyo0y3083er4fow": {
+        "title": "Nasion",
+        "symbol": "N"
+    },
+    "ckr6rysyc3jyq0y306afa3t9a": {
+        "title": "Orbitale",
+        "symbol": "Or"
+    },
+    "ckr6rysyd3jys0y30gd1rahjp": {
+        "title": "Pogonion",
+        "symbol": "Pog"
+    },
+    "ckr6rysyd3jyu0y30ah3205y8": {
+        "title": "Posterior Nasal Spine",
+        "symbol": "PNS"
+    },
+    "ckr6rysyd3jyw0y30hbbkfcb2": {
+        "title": "Pronasale",
+        "symbol": "Pn"
+    },
+    "ckr6rysye3jyy0y30h8n07rrl": {
+        "title": "Ramus",
+        "symbol": "R"
+    },
+    "ckr6rysye3jz00y30fzx36g43": {
+        "title": "Sella",
+        "symbol": "S"
+    },
+    "ckr6rysye3jz20y303mmleb3m": {
+        "title": "Articulare",
+        "symbol": "Ar"
+    },
+    "ckr6rysye3jz40y309hjl6dp2": {
+        "title": "Condylion",
+        "symbol": "Co"
+    },
+    "ckr6rysyf3jz60y30d6ir73i6": {
+        "title": "Gnathion",
+        "symbol": "Gn"
+    },
+    "ckr6rysyf3jz80y308ruj39bx": {
+        "title": "Gonion",
+        "symbol": "Go"
+    },
+    "ckr6rysyf3jza0y3010od7lu3": {
+        "title": "Porion",
+        "symbol": "Po"
+    },
+    "ckr6rysyf3jzc0y30bjsl17p5": {
+        "title": "Lower 2nd PM Cusp Tip",
+        "symbol": "LPM"
+    },
+    "ckr6rysyf3jze0y308g246gqv": {
+        "title": "Lower Incisor Tip",
+        "symbol": "LIT"
+    },
+    "ckr6rysyf3jzg0y3004pzauxj": {
+        "title": "Lower Molar Cusp Tip",
+        "symbol": "LMT"
+    },
+    "ckr6rysyf3jzi0y303zh895ny": {
+        "title": "Upper 2nd PM Cusp Tip",
+        "symbol": "UPM"
+    },
+    "ckr6rysyg3jzk0y303unk9msw": {
+        "title": "Upper Incisor Apex",
+        "symbol": "UIA"
+    },
+    "ckr6rysyg3jzm0y30e4r55szn": {
+        "title": "Upper Incisor Tip",
+        "symbol": "UIT"
+    },
+    "ckr6rysyg3jzo0y309pc56jgb": {
+        "title": "Upper Molar Cusp Tip",
+        "symbol": "UMT"
+    },
+    "ckr6rysyg3jzq0y309625913x": {
+        "title": "Lower Incisor Apex",
+        "symbol": "LIA"
+    },
+    "ckr6rysyg3jzs0y30533sccfw": {
+        "title": "Labrale inferius",
+        "symbol": "Li"
+    },
+    "ckr6rysyg3jzu0y308e25br5c": {
+        "title": "Labrale superius",
+        "symbol": "Ls"
+    },
+    "ckr6rysyg3jzw0y30cnaufc5r": {
+        "title": "Soft Tissue Nasion",
+        "symbol": "N`"
+    },
+    "ckr6rysyh3jzy0y30dqpoar9x": {
+        "title": "Soft Tissue Pogonion",
+        "symbol": "Pog`"
+    },
+    "ckr6rysyh3k000y302tpa6jn2": {
+        "title": "Subnasale",
+        "symbol": "Sn"
+    }
+}
+
+CVM_STAGES = {
+    "cl9ci0iy22jm807w12q3331m3": {
+        "title": "CVM-S1",
+        "value": 1
+    },
+    "cl9ci0iy22jma07w1fb137cme": {
+        "title": "CVM-S2",
+        "value": 2
+    },
+    "cl9ci0iy22jmc07w1fq1o53mi": {
+        "title": "CVM-S3",
+        "value": 3
+    },
+    "cl9ci0iy22jme07w142e7coez": {
+        "title": "CVM-S4",
+        "value": 4
+    },
+    "cl9ci0iy22jmg07w1eiu566n8": {
+        "title": "CVM-S5",
+        "value": 5
+    },
+    "cl9ci0iy22jmi07w13ibffvlh": {
+        "title": "CVM-S6",
+        "value": 6
+    }
+}
+
+NUM_LANDMARKS = 29
+NUM_CVM_STAGES = 6
